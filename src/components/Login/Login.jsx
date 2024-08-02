392f3de95e94626605e3c065e8393fa24105abb2
Muhammad Shoaib Mushtaq <shoaibfattani2010@gmail.com>
2024-08-03 00:04:46 +0500

    added responsiveness and resolved routing isssue using useNavigate hook from react router dom

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,16 @@
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
-import signup from "../../assets/signup.jpg";
+// import signup from "../../assets/signup.jpg";
 import "./Login.css";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [userName, setUserName] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
+  // const [isLogedin, setIsLogedin] = useState(false);
 
   const handleSignin = (e) => {
     e.preventDefault(); // Prevent the form from submitting and refreshing the page
@@ -25,7 +27,7 @@ const Login = () => {
       // If user is found, save the current user to local storage
       localStorage.setItem("currentUser", JSON.stringify(user));
       // Redirect to dashboard
-      window.location.href = "/dashboard";
+      navigate("/dashboard");
     } else {
       // If user is not found, set an error message
       setError("Invalid username or password");
@@ -33,46 +35,42 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-wrapper">
-        <div className="login-form">
-          <h1>Log In</h1>
-          {error && <p className="error-message">{error}</p>}
-          <form onSubmit={handleSignin}>
-            <div className="input-div">
-              <FaUser />
-              <input
-                type="text"
-                placeholder="Username"
-                className="input-box"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-div">
-              <RiLockPasswordLine />
-              <input
-                type="password"
-                placeholder="Password"
-                className="input-box"
-                value={userPassword}
-                onChange={(e) => setUserPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="remember-me">
-              <input type="checkbox" id="remember-me" />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
-            <button className="login-btn">Log in</button>
-          </form>
-        </div>
-        <div className="login-image">
-          <img src={signup} alt="Login" />
-          <Link to="/" className="signup-link">
-            Create an account
-          </Link>
+    <div className="login-wrapper">
+      <div className="login-form">
+        <h1>Log In</h1>
+        {error && <p className="error-message">{error}</p>}
+        <form onSubmit={handleSignin}>
+          <div className="input-div">
+            <FaUser />
+            <input
+              type="text"
+              placeholder="Username"
+              className="input-box"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-div">
+            <RiLockPasswordLine />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input-box"
+              value={userPassword}
+              onChange={(e) => setUserPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button className="login-btn1">Log in</button>
+        </form>
+        <div className="signup-div">
+          <p>Create an account</p>
+          <button className="signup-btn1">
+            <Link to="/" className="signup-link">
+              Sign up
+            </Link>
+          </button>
         </div>
       </div>
     </div>
--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -1,10 +1,10 @@
 import { MdOutlineMailOutline } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
-import signup from "../../assets/signup.jpg";
+// import signup from "../../assets/signup.jpg";
 import "./Signup.css";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const [userName, setUserName] = useState("");
@@ -12,6 +12,8 @@ const Signup = () => {
   const [userEmail, setUserEmail] = useState("");
   const [confirmPassword, setConfirmPassword] = useState();
   const [error, setError] = useState("");
+  // const [isSignedUp, setIsSignedUp] = useState(false);
+  const navigate = useNavigate();
 
   const handleSignup = (e) => {
     e.preventDefault(); // Prevent the form from submitting and refreshing the page
@@ -34,80 +36,76 @@ const Signup = () => {
     // Save the updated users array back to local storage
     localStorage.setItem("users", JSON.stringify(users));
 
-    alert("Signup successful!");
+    // setIsSignedUp(true);
 
-    // Redirect to signin page
-    window.location.href = "/login";
+    alert("Signup successfull");
+    setUserName("");
+    setUserEmail("");
+    setUserPassword("");
+    setConfirmPassword("");
+    setError("");
+    navigate("/login"); // Redirect to login page
   };
 
   return (
-    <div className="signup-container">
-      <div className="signup-wrapper">
-        <div className="signup-form">
-          <h1>Sign Up</h1>
-          <form onSubmit={handleSignup}>
-            <div className="input-div">
-              <FaUser />
-              <input
-                type="text"
-                placeholder="Username"
-                className="input-box"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-div">
-              <MdOutlineMailOutline />
-              <input
-                type="email"
-                placeholder="Your Email"
-                className="input-box"
-                value={userEmail}
-                onChange={(e) => setUserEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-div">
-              <RiLockPasswordLine />
-              <input
-                type="password"
-                placeholder="Password"
-                className="input-box"
-                value={userPassword}
-                onChange={(e) => setUserPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="input-div">
-              <RiLockPasswordLine />
-              <input
-                type="password"
-                placeholder="Repeat Your Password"
-                className="input-box"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-              {error && <p className="error-message">{error}</p>}
-            </div>
-            <div className="terms-checkbox">
-              <input type="checkbox" required />
-              <p>
-                I agree to all statements in{" "}
-                <strong style={{ textDecoration: "underline" }}>
-                  Terms of service
-                </strong>
-              </p>
-            </div>
-            <button className="signup-btn">Register</button>
-          </form>
-        </div>
-        <div className="signup-image">
-          <img src={signup} alt="Signup" />
-          <Link to="/login" className="login-link">
-            I am already a member
-          </Link>
+    <div className="signup-wrapper">
+      <div className="signup-form">
+        <h1>Sign Up</h1>
+        <form onSubmit={handleSignup}>
+          <div className="input-div">
+            <FaUser />
+            <input
+              type="text"
+              placeholder="Username"
+              className="input-box"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-div">
+            <MdOutlineMailOutline />
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="input-box"
+              value={userEmail}
+              onChange={(e) => setUserEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-div">
+            <RiLockPasswordLine />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input-box"
+              value={userPassword}
+              onChange={(e) => setUserPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-div">
+            <RiLockPasswordLine />
+            <input
+              type="password"
+              placeholder="Repeat Your Password"
+              className="input-box"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            {error && <p className="error-message">{error}</p>}
+          </div>
+          <button className="signup-btn">Register</button>
+        </form>
+        <div className="signin-div">
+          <p>I am already a member</p>
+          <button className="login-btn">
+            <Link to="/login" className="login-link">
+              Login
+            </Link>
+          </button>
         </div>
       </div>
     </div>
